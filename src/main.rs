c2ddac40827ee4ed3aa25de578607ba87cef0999
Andreas Maus <maus@badphish.ypbind.de>
2021-12-23 12:26:42 +0100

    Honor listen setting

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ mod mqtt;
 mod usage;
 
 use getopts::Options;
+use log::error;
+use std::net::ToSocketAddrs;
 use std::sync::mpsc;
-use std::thread;
-use std::{env, process};
+use std::{env, process, thread};
 use warp::Filter;
 
 #[tokio::main]
@@ -86,18 +87,50 @@ async fn main() {
         gather::run(receive, td_recv_svc);
     });
 
+    // start MQTT thread
     let mqtt_cfg = config.mqtt;
     let mqtt_thread_id = thread::spawn(move || {
         mqtt::start_mqtt_client(&mqtt_cfg, send);
     });
+
+    // Start HTTP thread
+    let svc_cfg = match config.service {
+        Some(v) => v,
+        None => panic!("BUG: main: config.service is undefined"),
+    };
+
+    // TODO: process server. metrics_path here
+    let metrics_path = match &svc_cfg.metrics_path {
+        Some(v) => v.trim_start_matches('/').trim_end_matches('/'),
+        None => panic!("BUG: config.service.metrics_path is undefined"),
+    };
+
+    let listen = match &svc_cfg.listen {
+        Some(v) => v,
+        None => panic!("BUG: main: config.service.listen is undefined"),
+    };
+    let sockaddrs = match listen.to_socket_addrs() {
+        Ok(v) => v,
+        Err(e) => {
+            error!("Can't resolve listener address: {}", e);
+            process::exit(1);
+        }
+    };
+    let addresses: Vec<_> = sockaddrs.collect();
+    if addresses.is_empty() {
+        error!("Can't resolve listener address");
+        process::exit(1);
+    }
+    let socketaddr = addresses[0];
 
     // TODO: process server. metrics_path here
     let metrics = warp::path!("metrics")
         .and(warp::get())
         .map(gather::serve_metrics);
 
-    // TODO: process server.listen here
-    warp::serve(metrics).run(([127, 0, 0, 1], 6883)).await;
+    // XXX: async rust with tokio might provide a better solution enable graceful shutdown
+    //      e.g. https://docs.rs/warp/latest/warp/struct.Server.html#method.bind_with_graceful_shutdown
+    warp::serve(metrics).run(socketaddr).await;
 
     // TODO: How to shutdown MQTT loop gracefully ?
     #[allow(unused_must_use)]
